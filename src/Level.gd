eae19faf6f1155e08a58dbb54bf404ee345c20ca
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-02-05 01:37:49 +0200

    Add animation string in the Checkpoint class

--- src/Level.gd
+++ src/Level.gd
@@ -3,22 +3,22 @@ extends Node2D
 var obstacle_tscn := preload("res://src/obstacle.tscn")
 var tree_tscn := preload("res://src/tree.tscn")
 var checkpoints: Array[Checkpoint] = [
-	Checkpoint.new(0, "The seed flew east, through a forest. (press [space] to jump)", "(forest ambience)"),
-	Checkpoint.new(700, "Navigating its way through the forest, the seed had to avoid the tree tops"),
-	Checkpoint.new(1200, "“This forest is way too dense” the seed thought. I won’t be able to root properly here, with so little sun."),
-	Checkpoint.new(1700, "But then, the seed lost its train of thought to a cracking sound.", "(sound of a tree being chopped and sound of trees falling)"),
-	Checkpoint.new(2000, "Why are the trees falling out of nowhere? Maybe that’s my chance to root!"),
-	Checkpoint.new(3000, "But as the seed navigated further into the forest it realised that this place is far from safe.", "(sounds of rocks and arrows+new obstacles)"),
-	Checkpoint.new(4000, "The trees were falling one after the other. What could have caused such a catastrophe?"),
-	Checkpoint.new(5000, "I have to travel further, the seed thought and it gathered all of its strength and courage to go even further."),
-	Checkpoint.new(6000, "But things were getting even more weird the further it went. The forest lost its colours and the sound became louder and louder", "(sounds of a busy city)"),
-	Checkpoint.new(7000, "“There’s barely any soil here, how will I find a place to root?” The seed thought as it traveled even further in that gray looking forest."),
-	Checkpoint.new(8000, "And suddenly, a sound unlike any other.", "(bomb falling, exploding)"),
-	Checkpoint.new(9000, "And everything was calm again.", "(sound of fire)"),
-	Checkpoint.new(10000, "The forest was long gone."),
-	Checkpoint.new(11000, "But the poor seed had little strength, for it had used all of its energy looking for a better place to root."),
-	Checkpoint.new(12000, "Maybe this place were never meant to be found. Because of a greedy species with zero consideration for nature or even its own kind was a bad encounter in the seed’s adventure that it shouldn’t have to have."),
-	Checkpoint.new(13000, "A parasite, here only to destroy. Periods of conflict followed, leaving fire and blood at their trails. Hope was naught but a faint light, yet wars were in humanity’s nature, never to be stopped."),
+	Checkpoint.new(0, "", "The seed flew east, through a forest. (press [space] to jump)", "(forest ambience)"),
+	Checkpoint.new(700, "forest_fade_1", "Navigating its way through the forest, the seed had to avoid the tree tops"),
+	Checkpoint.new(1200, "forest_fade_2", "“This forest is way too dense” the seed thought. I won’t be able to root properly here, with so little sun."),
+	Checkpoint.new(1700, "town_fade_in_1", "But then, the seed lost its train of thought to a cracking sound.", "(sound of a tree being chopped and sound of trees falling)"),
+	Checkpoint.new(2000, "", "Why are the trees falling out of nowhere? Maybe that’s my chance to root!"),
+	Checkpoint.new(3000, "", "But as the seed navigated further into the forest it realised that this place is far from safe.", "(sounds of rocks and arrows+new obstacles)"),
+	Checkpoint.new(4000, "", "The trees were falling one after the other. What could have caused such a catastrophe?"),
+	Checkpoint.new(5000, "", "I have to travel further, the seed thought and it gathered all of its strength and courage to go even further."),
+	Checkpoint.new(6000, "", "But things were getting even more weird the further it went. The forest lost its colours and the sound became louder and louder", "(sounds of a busy city)"),
+	Checkpoint.new(7000, "", "“There’s barely any soil here, how will I find a place to root?” The seed thought as it traveled even further in that gray looking forest."),
+	Checkpoint.new(8000, "", "And suddenly, a sound unlike any other.", "(bomb falling, exploding)"),
+	Checkpoint.new(9000, "", "And everything was calm again.", "(sound of fire)"),
+	Checkpoint.new(10000, "", "The forest was long gone."),
+	Checkpoint.new(11000, "", "But the poor seed had little strength, for it had used all of its energy looking for a better place to root."),
+	Checkpoint.new(12000, "", "Maybe this place were never meant to be found. Because of a greedy species with zero consideration for nature or even its own kind was a bad encounter in the seed’s adventure that it shouldn’t have to have."),
+	Checkpoint.new(13000, "", "A parasite, here only to destroy. Periods of conflict followed, leaving fire and blood at their trails. Hope was naught but a faint light, yet wars were in humanity’s nature, never to be stopped."),
 ]
 var current_checkpoint := 0
 @onready var player: Player = $Player
@@ -30,11 +30,13 @@ var current_checkpoint := 0
 
 class Checkpoint:
 	var pos := 0.0
+	var animation := ""
 	var text := ""
 	var ambient_text := ""
 
-	func _init(_pos: float, _text := "", _ambient_text := "") -> void:
+	func _init(_pos: float, _animation := "", _text := "", _ambient_text := "") -> void:
 		pos = _pos
+		animation = _animation
 		text = _text
 		ambient_text = _ambient_text
 
@@ -58,15 +60,11 @@ func _process(_delta: float) -> void:
 
 func change_checkpoint() -> void:
 	GameManager.save(current_checkpoint)
+	if not checkpoints[current_checkpoint].animation.is_empty():
+		animation_player.play(checkpoints[current_checkpoint].animation)
 	match current_checkpoint:
 		0:
 			tree_timer.start()
-		1:
-			animation_player.play("forest_fade_1")
-		2:
-			animation_player.play("forest_fade_2")
-		3:
-			animation_player.play("town_fade_in_1")
 	subtitles.text = checkpoints[current_checkpoint].text
 	if GameManager.show_ambient_subtitles:
 		subtitles.text += "\n" + checkpoints[current_checkpoint].ambient_text
