415225ecdcc8130a838f3425a72809b7dd26e66f
Manolis Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2023-02-04 14:14:24 +0200

    Obstacke curve movement

--- src/level_test.gd
+++ src/level_test.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 var obstacle_tscn := preload("res://src/obstacle.tscn")
+@onready var player: Player = $Player
 
 
 func _on_timer_timeout() -> void:
 	var obstacle: Obstacle = obstacle_tscn.instantiate()
-	obstacle.position = Vector2(800, 400)
+	var pos := Vector2(player.position.x + 940, 720)
+	obstacle.position = pos
 	add_child(obstacle)
--- src/obstacle.gd
+++ src/obstacle.gd
@@ -1,14 +1,28 @@
 class_name Obstacle
-extends Area2D
+extends CharacterBody2D
 
-var speed := 300.0
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity := ProjectSettings.get_setting("physics/2d/default_gravity")
+var speed := -40000.0
+var jump_velocity = -40000.0
+var has_jumped := false
+@onready var path_follow_2d: PathFollow2D = $Path2D/PathFollow2D
+
+
+func _ready() -> void:
+	jump_velocity = randf_range(-80000, -40000)
 
 
 func _physics_process(delta: float) -> void:
-	position.x -= speed * delta
-	position.y += speed * delta
+	velocity.y += gravity * delta
+	if not has_jumped:
+		has_jumped = true
+		velocity.y = jump_velocity * delta
+
+	velocity.x = speed * delta
+	move_and_slide()
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		print("Game over")
