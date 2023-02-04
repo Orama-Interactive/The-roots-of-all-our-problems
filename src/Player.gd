cdecb87a5a0cc342e15395a26c1597b4ed5e127e
Manolis Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2023-02-04 13:27:06 +0200

    Basic parallax backgrounds & change scaling method & remove unused player code

--- src/Player.gd
+++ src/Player.gd
@@ -18,12 +18,4 @@ func _physics_process(delta: float):
 		velocity.y = JUMP_VELOCITY
 
 	velocity.x = SPEED * delta
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-#	var direction := Input.get_axis("ui_left", "ui_right")
-#	if direction:
-#		velocity.x = direction * SPEED
-#	else:
-#		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
