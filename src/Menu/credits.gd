2247607e1fc4562cc4a14faed62bf8ea605621b1
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-02-05 10:07:34 +0200

    Update credits

--- src/Menu/credits.gd
+++ src/Menu/credits.gd
@@ -1,5 +1,11 @@
 extends Control
 
+@export var scroll_speed := 40.0
+@onready var header: Label = $Header
+@onready var by_orama: Label = $ByOrama
+@onready var text: RichTextLabel = $Text
+
+
 
 func _input(event: InputEvent) -> void:
 	if (event is InputEventKey
@@ -7,3 +13,10 @@ func _input(event: InputEvent) -> void:
 	or event is InputEventScreenTouch
 	or event is InputEventJoypadButton):
 		get_tree().change_scene_to_file("res://src/Menu/menu.tscn")
+
+
+func _process(delta: float) -> void:
+	header.position.y -= scroll_speed * delta
+	by_orama.position.y -= scroll_speed * delta
+	text.position.y -= scroll_speed * delta
+	text.size.y += scroll_speed * delta
