8040067cc64a0d71ac993a331b48964360de5c0f
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-02-05 08:20:57 +0200

    Floor & ceiling obstacles

--- src/Obstacles/obstacle.gd
+++ src/Obstacles/obstacle.gd
@@ -1,17 +1,23 @@
 class_name Obstacle
 extends CharacterBody2D
 
+enum Place { FLOOR, CEILING }
+
+@export var place := Place.FLOOR
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity := ProjectSettings.get_setting("physics/2d/default_gravity")
 var speed := -60000.0
 var jump_velocity = -40000.0
 var has_jumped := false
 var player_pos := Vector2.ZERO
+@onready var sprite: Sprite2D = $Area2D/Sprite
 
 
 func _ready() -> void:
+	var tex_size := sprite.texture.get_size()
 	position.x = player_pos.x + 1940
-	position.y = 1080
+	position.y = 1080.0 if place == Place.FLOOR else tex_size.y
 
 
 func _physics_process(delta: float) -> void:
