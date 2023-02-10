fc250f9f7028334b61909e63ab30ee752b031bf7
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-02-10 02:08:27 +0200

    Player lose sound

--- src/Player.gd
+++ src/Player.gd
@@ -5,14 +5,16 @@ const SPEED := 10000.0
 const JUMP_VELOCITY: = -400.0
 
 var can_move := true
-var falling_sound := preload("res://assets/audio/sounds/dragonfly_2.wav")
-var flying_sound := preload("res://assets/audio/sounds/dragonfly_3.wav")
+var flying_sound := preload("res://assets/audio/sounds/dragonfly_2.wav")
+var flying_sound_2 := preload("res://assets/audio/sounds/dragonfly_3.wav")
+var falling_sound := preload("res://assets/audio/sounds/player_fall.mp3")
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var falling := false
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 var record_bus_index: int
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var falling_sound_player: AudioStreamPlayer = $FallingSoundPlayer
 @onready var camera_2d: Camera2D = $Camera2D
 
 
@@ -28,24 +30,23 @@ func start() -> void:
 func _physics_process(delta: float):
 	if not can_move:
 		return
-	# Add the gravity.
+
 	velocity.y += gravity * delta
 	velocity.y = clamp(velocity.y, JUMP_VELOCITY, 1200)
 	var mic_input := get_mic_input() > 0.2 if GameManager.play_with_voice else false
 	var jump := Input.is_action_pressed("jump") or mic_input
-
-	# Handle Jump.
-	if not falling and jump:
-		velocity.y = JUMP_VELOCITY
-		if animated_sprite_2d.speed_scale != 3:
-			animated_sprite_2d.speed_scale = 3
-			audio_stream_player.stream = flying_sound
-			audio_stream_player.play()
-	else:
-		if animated_sprite_2d.speed_scale != 1:
-			animated_sprite_2d.speed_scale = 1
-			audio_stream_player.stream = falling_sound
-			audio_stream_player.play()
+	if not falling:
+		if jump:
+			velocity.y = JUMP_VELOCITY
+			if animated_sprite_2d.speed_scale != 3:
+				animated_sprite_2d.speed_scale = 3
+				audio_stream_player.stream = flying_sound_2
+				audio_stream_player.play()
+		else:
+			if animated_sprite_2d.speed_scale != 1:
+				animated_sprite_2d.speed_scale = 1
+				audio_stream_player.stream = flying_sound
+				audio_stream_player.play()
 
 	velocity.x = SPEED * delta
 	move_and_slide()
@@ -62,5 +63,7 @@ func get_mic_input() -> float:
 
 func fall() -> void:
 	if not falling:
+		falling_sound_player.stream = falling_sound
+		falling_sound_player.play()
 		animated_sprite_2d.play("fall")
 		falling = true
