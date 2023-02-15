8a598613680cb09debbd347168b58b18dc38fb75
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2023-02-15 20:56:16 +0200

    New music (#7)
    * Added Menu and Intro music. Created a Tween to handle menu to intro transition
    
    * Did a minor change to the intro music
    
    * Added gameplay music, added old intro music to the game as the main menu music
    
    ---------
    
    Co-authored-by: Chrd26 <christosdouris@outlook.com>

--- src/Menu/menu.gd
+++ src/Menu/menu.gd
@@ -3,16 +3,23 @@ extends Control
 @onready var buttons: VBoxContainer = $Buttons
 @onready var new_button: Button = $Buttons/New
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-
+@onready var menuMusic: AudioStreamPlayer = $menuMusic
+@onready var introMusic: AudioStreamPlayer = $introMusic
+var isIntroPlaying = false
 
 func _ready() -> void:
 	new_button.grab_focus()
 
+func _process(_delta):
+	if (menuMusic.volume_db <= -25 && !isIntroPlaying):
+		PlayIntroMusic()
 
 func _on_new_pressed() -> void:
 	animation_player.play("first_scene")
 	for button in buttons.get_children():
 		button.disabled = true
+	var menuMusicTween = get_tree().create_tween()
+	menuMusicTween.tween_property(menuMusic,"volume_db",-100,5.5)
 
 
 func _on_load_pressed() -> void:
@@ -38,3 +45,8 @@ func _on_credits_pressed() -> void:
 
 func _on_skip_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/Level.tscn")
+
+func PlayIntroMusic():
+	menuMusic.stop()
+	introMusic.play()
+	isIntroPlaying = true
