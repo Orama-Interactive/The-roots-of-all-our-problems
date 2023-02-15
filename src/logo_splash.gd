2f2816eef59c2c6dd8130dc2543e1a933f4b0e04
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-02-15 23:03:34 +0200

    Add a logo splash screen

---
+++ src/logo_splash.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _ready() -> void:
+	$AnimationPlayer.play("splash")
+
+
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+	get_tree().change_scene_to_file("res://src/Menu/menu.tscn")
