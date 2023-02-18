a350b33698c7133f33a30bda63a54a938d28d417
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-02-19 00:40:54 +0200

    Improve logo splash animation and skip it if anything is pressed

--- src/logo_splash.gd
+++ src/logo_splash.gd
@@ -5,5 +5,10 @@ func _ready() -> void:
 	$AnimationPlayer.play("splash")
 
 
+func _input(event: InputEvent) -> void:
+	if Input.is_anything_pressed():
+		get_tree().change_scene_to_file("res://src/Menu/menu.tscn")
+
+
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	get_tree().change_scene_to_file("res://src/Menu/menu.tscn")
