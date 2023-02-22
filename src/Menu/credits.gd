bef6ff2508c91ddf091129d43085d125b3ad261c
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-02-22 17:53:16 +0200

    Add a fade effect to the credits

--- src/Menu/credits.gd
+++ src/Menu/credits.gd
@@ -2,12 +2,13 @@ extends Control
 
 @export var scroll_speed := 40.0
 @onready var header: Label = $Header
-@onready var by_orama: Label = $ByOrama
 @onready var text: RichTextLabel = $Text
+@onready var fade_in: ColorRect = $FadeIn
 
 
 func _ready() -> void:
 	text.text = tr(text.text)
+	create_tween().tween_property(fade_in, "modulate", Color(1, 1, 1, 0), 1)
 
 
 func _input(event: InputEvent) -> void:
@@ -15,11 +16,15 @@ func _input(event: InputEvent) -> void:
 	or event is InputEventMouseButton
 	or event is InputEventScreenTouch
 	or event is InputEventJoypadButton):
-		get_tree().change_scene_to_file("res://src/Menu/menu.tscn")
+		create_tween().tween_property(fade_in, "modulate", Color(1, 1, 1, 1), 1).finished.connect(_go_to_menu)
 
 
 func _process(delta: float) -> void:
 	header.position.y -= scroll_speed * delta
-	by_orama.position.y -= scroll_speed * delta
 	text.position.y -= scroll_speed * delta
 	text.size.y += scroll_speed * delta
+
+
+func _go_to_menu() -> void:
+	GameManager.menu_faded_in = false
+	get_tree().change_scene_to_file("res://src/Menu/menu.tscn")
