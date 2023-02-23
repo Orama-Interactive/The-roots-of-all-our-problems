e1fdbed3994fb0252875a97c72e92b1678b23eaf
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-02-23 02:21:56 +0200

    Change the white bars in the ending to fit properly in Greek
    Although we should find a better solution to hide the other words, this one would require manual intervention if we change the dialogue or add more languages.

--- src/ending.gd
+++ src/ending.gd
@@ -3,6 +3,11 @@ extends Control
 
 func _ready() -> void:
 	$AnimationPlayer.play("ending")
+	if get_theme_default_font().font_name == "Mandy's Sketch":
+		$WhiteBars/ColorRect2.position = Vector2i(175, 893)
+		$WhiteBars/ColorRect2.size = Vector2i(67, 55)
+		$WhiteBars/ColorRect3.position = Vector2i(988, 889)
+		$WhiteBars/ColorRect3.size = Vector2i(62, 55)
 
 
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
