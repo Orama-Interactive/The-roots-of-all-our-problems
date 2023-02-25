faae3800322d682bd3dcf7b8766986ce539760aa
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-02-25 22:27:02 +0200

    Fullscreen toggle shortcut

--- src/Autoloads/GameManager.gd
+++ src/Autoloads/GameManager.gd
@@ -17,6 +17,11 @@ var tutorial_shown := false
 
 
 func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fullscreen"):
+		if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	if get_tree().current_scene.name != "Level":
 		return
 	if event.is_action_pressed("pause"):
